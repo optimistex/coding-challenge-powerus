e125f2ceab1a0e78d00b9bf919fbfe2f6fbebc75
optimistex <optimistex@gmail.com>
2022-06-12 01:23:15 +0300

    delete: unnecessary code

--- src/flight-aggregator/interface/flight-aggregator-cache.interface.ts
+++ src/flight-aggregator/interface/flight-aggregator-cache.interface.ts
@@ -5,11 +5,6 @@ export interface CustomCacheOptions {
 
 export interface FlightAggregatorCache {
   /**
-   * Make cache key as a hash from any data
-   */
-  cacheKey(data: object): string;
-
-  /**
    * Get data by a key
    */
   get<T>(key: string, defaultValue?: T): T;
@@ -18,9 +13,4 @@ export interface FlightAggregatorCache {
    * Set cache for maxAge seconds
    */
   set<T>(key: string, value: T, options: CustomCacheOptions): void;
-
-  /**
-   * Remove all data by a tag
-   */
-  clearCache(tags: string[]): void;
 }
