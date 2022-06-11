29e658cfa40c1256e23430f5224bdd60572dccea
optimistex <optimistex@gmail.com>
2022-06-12 01:09:02 +0300

    fix: e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,7 @@
+import { of } from 'rxjs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
@@ -9,16 +11,16 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(HttpService)
+      .useValue({ get: jest.fn().mockReturnValue(of(undefined)) })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect([]);
   });
 });
