0d4228ccb4b2d3024d8f0caf989a83182d13112a
optimistex <optimistex@gmail.com>
2022-06-12 00:09:54 +0300

    add: tests of FlightAggregatorService

--- src/flight-aggregator/flight-aggregator/flight-aggregator.service.spec.ts
+++ src/flight-aggregator/flight-aggregator/flight-aggregator.service.spec.ts
@@ -1,24 +1,56 @@
+import { of, PartialObserver } from 'rxjs';
 import { Provider } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FLIGHT_AGGREGATOR_MODULE_OPTIONS, FlightAggregatorModuleOptions } from '../flight-aggregator.module-options';
 import { FlightSourceHandler } from '../flight-source-handler/flight-source-handler';
+import { Flight } from '../interface/flight-data.interface';
 import { FlightAggregatorService } from './flight-aggregator.service';
+
+function mockFlightSourceHandler(value: Partial<Flight>[]): FlightSourceHandler {
+  return { getFlights: jest.fn().mockReturnValue(of(value)) } as Partial<FlightSourceHandler> as FlightSourceHandler;
+}
 
 describe('FlightAggregatorService', () => {
   let service: FlightAggregatorService;
-  let flightSources: FlightSourceHandler[];
+  let mockOptions: FlightAggregatorModuleOptions;
+  let spyObserver: jest.Mocked<PartialObserver<unknown>>;
 
   beforeEach(async () => {
-    flightSources = [];
-    const mockOptionsProvider: Provider<FlightAggregatorModuleOptions> = { provide: FLIGHT_AGGREGATOR_MODULE_OPTIONS, useValue: { flightSources } };
+    mockOptions = { flightSources: [] };
+    const mockOptionsProvider: Provider<FlightAggregatorModuleOptions> = { provide: FLIGHT_AGGREGATOR_MODULE_OPTIONS, useValue: mockOptions };
     const module: TestingModule = await Test.createTestingModule({
       providers: [FlightAggregatorService, mockOptionsProvider],
     }).compile();
 
     service = module.get<FlightAggregatorService>(FlightAggregatorService);
+    spyObserver = { next: jest.fn(), error: jest.fn() };
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should merge data', () => {
+    mockOptions.flightSources = [
+      mockFlightSourceHandler([{ id: 'flight1' }, { id: 'flight2' }]),
+      mockFlightSourceHandler([{ id: 'flight3' }, { id: 'flight4' }]),
+    ];
+    service.getFlights().subscribe(spyObserver);
+
+    expect(spyObserver.next).toBeCalledTimes(1);
+    expect(spyObserver.next).toBeCalledWith([{ id: 'flight1' }, { id: 'flight2' }, { id: 'flight3' }, { id: 'flight4' }]);
+    expect(spyObserver.error).not.toBeCalled();
+  });
+
+  it('should remove duplicates', () => {
+    mockOptions.flightSources = [
+      mockFlightSourceHandler([{ id: 'flight1' }, { id: 'flight2' }, { id: 'flight3' }]),
+      mockFlightSourceHandler([{ id: 'flight3' }, { id: 'flight2' }, { id: 'flight4' }]),
+    ];
+    service.getFlights().subscribe(spyObserver);
+
+    expect(spyObserver.next).toBeCalledTimes(1);
+    expect(spyObserver.next).toBeCalledWith([{ id: 'flight1' }, { id: 'flight2' }, { id: 'flight3' }, { id: 'flight4' }]);
+    expect(spyObserver.error).not.toBeCalled();
   });
 });
--- src/flight-aggregator/flight-aggregator/flight-aggregator.service.ts
+++ src/flight-aggregator/flight-aggregator/flight-aggregator.service.ts
@@ -1,14 +1,18 @@
-import { zip } from 'rxjs';
+import { map, Observable, zip } from 'rxjs';
 import { Inject, Injectable } from '@nestjs/common';
 import { FLIGHT_AGGREGATOR_MODULE_OPTIONS, FlightAggregatorModuleOptions } from '../flight-aggregator.module-options';
+import { Flight } from '../interface/flight-data.interface';
 
 @Injectable()
 export class FlightAggregatorService {
-  constructor(@Inject(FLIGHT_AGGREGATOR_MODULE_OPTIONS) private options: FlightAggregatorModuleOptions) {
-    console.log('FlightAggregatorService', this.options);
-  }
+  constructor(@Inject(FLIGHT_AGGREGATOR_MODULE_OPTIONS) private options: FlightAggregatorModuleOptions) {}
 
-  public getFlights() {
-    return zip(this.options.flightSources.map((s) => s.getFlights())).pipe();
+  public getFlights(): Observable<Flight[]> {
+    return zip(this.options.flightSources.map((s) => s.getFlights())).pipe(
+      map((data) => {
+        const hashList: Record<string, boolean> = {};
+        return [].concat(...data).filter((flight) => (hashList[flight.id] ? false : (hashList[flight.id] = true)));
+      }),
+    );
   }
 }
